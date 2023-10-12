ed20464b57db08e4a9f1f7809c4fea16b2de04f5
kalyan6013 <kalyan6013@gmail.com>
2023-10-12 12:17:57 +0530

    Node App

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { addUser, getUsers} = require("./controllers/user");
+
+const app = express();
+const PORT = process.env.PORT || 4000
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/user", getUsers);
+app.post("/user", addUser);
+
+app.get('/', (req, res) => {
+    res.send({ message: `Server is running on ${PORT} ENV ${JSON.stringify(process.env)}` });
+});
+app.listen(PORT, () => {
+    console.log(`Application starting on port ${PORT}`);
+});
